833868f323d92a447c30e199e8ceaa8062ac32cf
Joseph-ADAMAH <adamahkouevijoseph@gmail.com>
2025-10-31 08:29:08 +0000

    Ajout de la Redmee.md

--- CodeQr.py
+++ CodeQr.py
@@ -1,16 +1,19 @@
 # Importation du module qrcode
 import qrcode
+
 # Message expliquant le lien du site
 message = """Voici un site que tout étudiant en informatique dois connaitre 
  https://www.w3schools.com/default.asp"""
+
 # Création de l'objet code sur lequel je vais appeler les différentes méthodes
+Code = qrcode.QRCode(version =1, box_size = 5, border = 3)
+
 """
 Explication des paramètre:
-- Version : Permet de définir la caoacité du code QR 
+- Version : Permet de définir la capacité du code QR 
 - box_size : permet de définir la taille de l'image du code QR 
 - border : permet de définir la taille de la couleur blanche autour du code QR 
 """
-Code = qrcode.QRCode(version =1, box_size = 5, border = 3)
 Code.add_data(message) # Ajouetr du message au code QR
 Code.make(fit = True)
 img = Code.make_image(fill_color = "black", back_color = "white")
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# codeQR-python
+# 🌏🌏🌏 codeQR-python
+
+----
+## Description du projet :
+
+-------------
+
+Ceci est un petit projet qui consiste à créer un code QR avec python
+
+## Outils
+Pour ce projet nous devons installer le module qrcode avec la comande : ***pip install qrcode pillow***
+
+## ✍️Explication du code : 
+- *Code = qrcode.QRCode(version =1, box_size = 5, border = 3)* : Création de l'objet code sur lequel on va appeler les différentes méthodes
+- *Code.add_data(message)*: Ajout des informations du code QR
+- *Code.make(fit = True)* : Déclenchement la génération du QR code.
+
+![L'image](CodeQr.png)
+
+[Poste Linkdin]()
+
+
+
